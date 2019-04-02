831d2d36f7e994616cffe7e3bebe9dc93f44abe6
Tomek Stacharski <twtstacharski@gmail.com>
2019-04-02 08:08:32 +0200

    API fetch implemented. And POST added too in order to actually have some stuff to work with

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,43 @@
 // TODO: add and export your own actions
-
 export const FETCH_CARS = 'FETCH_CARS';
+export const POST_CAR = 'POST_CAR';
+
+const URL = 'https://wagon-garage-api.herokuapp.com/tomek/cars';
+
+  // const cars = [
+  //   { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+  //   { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+  //   { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+  //   { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+  // ];
 
 export function fetchCars() {
-  // const url
-  // const promise = fetch(url).then(r => r.json());
-  console.log('fetchCars called')
-  const cars = [
-    { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
-    { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
-    { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
-    { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
-  ];
+  const promise = fetch(URL).then(r => r.json());
 
   return {
     type: FETCH_CARS,
-    payload: cars // Will be resolved by redux-promise
+    payload: promise // Will be resolved by redux-promise
+  };
+}
+
+export function postCar(brand, model, owner, plate) {
+  const body = {
+    brand: brand,
+    model: model,
+    owner: owner,
+    plate: plate };
+
+  const promise = fetch(URL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(r => r.json());
+
+  return {
+    type: POST_CAR,
+    payload: promise // Will be resolved by redux-promise
   };
 }
